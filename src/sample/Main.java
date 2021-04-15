0647ba64cd387e7e09f63f9acd5288e87ed93665
cerberus <cerberusprogrammer@gmail.com>
2021-04-14 18:43:07 -0700

    tht

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1400, 720));
+        primaryStage.setTitle("Practica #4 - Colas - Omar Flores");
+        primaryStage.setScene(new Scene(root, 1800, 720));
         primaryStage.show();
     }
 
